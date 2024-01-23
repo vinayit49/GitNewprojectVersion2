dccbc985971d5a84118b95516fee00ef6a8c34f1
vinay <vinayit716@gmail.com>
2024-01-23 16:54:55 +0530

    Intial commit

---
+++ src/Lashika/Test.java
@@ -0,0 +1,18 @@
+package Lashika;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String input="Vinayvani";
+		char[] chars=input.toCharArray();
+		int length=chars.length;
+		for(int i=length-1;i>=0;--i)
+		{
+			System.out.print(chars[i]);
+		}
+		   
+
+	}
+
+}
